0388e3c7270436aed4e8bc79706091d2d3a198bc
Alex Lamb <lambalex@leto50.iro.umontreal.ca>
2016-03-13 21:42:04 -0400

    Changes

--- lib/autoencoder.py
+++ lib/autoencoder.py
@@ -1,29 +1,34 @@
 import Data
 import lasagne
 import numpy as np
+import theano
+import theano.tensor as T
 
 def init_params(config):
     params = {}
 
-    params["W_enc_1"] = theano.shared(np.random.normal(size = (4000, 2048))
-    params["W_enc_2"] = 
+    scale = 0.1
+    num_latent = 128
 
-    params["b_enc_1"] = 
-    params["b_enc_2"] = 
+    params["W_enc_1"] = theano.shared(scale * np.random.normal(size = (4000, 2048)).astype('float32'))
+    params["W_enc_2"] = theano.shared(scale * np.random.normal(size = (2048, 2048)).astype('float32'))
 
-    params["z_mean_W"] = 
-    params["z_mean_b"] = 
+    params["b_enc_1"] = theano.shared(0.0 * np.random.normal(size = (2048)).astype('float32'))
+    params["b_enc_2"] = theano.shared(0.0 * np.random.normal(size = (2048)).astype('float32'))
 
-    params["z_std_W"] =
-    params["z_std_b"] = 
+    params["z_mean_W"] = theano.shared(scale * np.random.normal(size = (2048, num_latent)).astype('float32'))
+    params["z_mean_b"] = theano.shared(0.0 * np.random.normal(size = (num_latent)).astype('float32'))
 
-    params['W_dec_1'] = 
-    params['W_dec_2'] = 
-    params['W_dec_3'] = 
+    params["z_std_W"] = theano.shared(scale * np.random.normal(size = (2048, num_latent)).astype('float32'))
+    params["z_std_b"] = theano.shared(0.0 * np.random.normal(size = (num_latent)).astype('float32'))
 
-    params['b_dec_1'] = 
-    params['b_dec_2'] = 
-    params['b_dec_3'] = 
+    params['W_dec_1'] = theano.shared(scale * np.random.normal(size = (num_latent, 2048)).astype('float32'))
+    params['W_dec_2'] = theano.shared(scale * np.random.normal(size = (2048, 2048)).astype('float32'))
+    params['W_dec_3'] = theano.shared(scale * np.random.normal(size = (2048, 4000)).astype('float32'))
+
+    params['b_dec_1'] = theano.shared(0.0 * np.random.normal(size = (2048)).astype('float32'))
+    params['b_dec_2'] = theano.shared(0.0 * np.random.normal(size = (2048)).astype('float32'))
+    params['b_dec_3'] = theano.shared(0.0 * np.random.normal(size = (4000)).astype('float32'))
 
     return params
 
@@ -32,20 +37,37 @@ Maps from a given x to an h_value.
 
 
 '''
-def encoder(params):
+def encoder(x, params):
     pass
 
+'''
+Maps from a given z to a decoded x.  
 
-def decoder(params):
+'''
+def decoder(z, params):
     pass
 
+'''
+Given x (uunormalized), returns a reconstructed_x and a sampled x (both unnormalized)
+'''
 
-
-def define_network(x):
+def define_network(x, params):
     pass
 
 
 if __name__ == "__main__":
+
+
+    params = init_params({})
+
+    x = T.matrix()
+
+    res = define_network(x, params)
+
+    #estimate z_sampled, z_reconstruction
+
+
+
 
 
 
